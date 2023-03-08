403b4e88a99574d2b794c679362d2a451d45c4d3
vkutin12q <thinhnp.22git@vku.udn.vn>
2023-03-08 17:27:13 +0700

    Initial commit

---
+++ Client/src/main/java/com/example/client/MainController.java
@@ -0,0 +1,12 @@
+package com.example.client;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class MainController {
+    @FXML
+    private VBox vB;
+
+
+}
